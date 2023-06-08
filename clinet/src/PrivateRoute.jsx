ed76cb2c22b6f06173e1cb5e3609063ee78d488f
mohamadreza kiani <mohamadrezakiani9@yahoo.com>
2023-06-09 00:13:29 +0330

    edit files

--- clinet/src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-function PrivateRoute({ address, children }) {
-  if (!address) {
-    return <Navigate to="/" replace />
-  }
-  return children
-}
-export default PrivateRoute
