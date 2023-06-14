66e58c39403ee3c3785ddd5dffd09dc02857988a
mohamadreza kiani <mohamadrezakiani9@yahoo.com>
2023-06-15 01:40:10 +0330

    add new methods

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -13,10 +13,11 @@ import {
   pinFileToIPFS,
   pinata_api_key1,
   pinata_secret_api_key1,
-} from "./NFT/ipfs";
+} from "./utils/ipfs";
 import Table from "./components/Table.jsx";
 import ModalAdd from "./components/ModalAdd";
 import ModalTransfer from "./components/ModalTransfer";
+import ModalAddOwner from "./components/Modaladdoperator";
 function Dashboard() {
   const [studentRegistryContract, setContract] = useState(null);
   const [nftContract, setContract2] = useState(null);
@@ -31,6 +32,7 @@ function Dashboard() {
   const [from, setFrom] = useState("");
   const [to, setTo] = useState("");
   const [tokenId, setTokenId] = useState("");
+  const [address, setaddress] = useState("");
 
   const { data } = useContract(
     contrcatAddress.StudentRegistryContractAddress,
@@ -50,7 +52,7 @@ function Dashboard() {
     });
     return false;
   }
-
+ 
   useEffect(() => {
     if (studentRegistryContract != null) {
       handleReadStudent();
@@ -87,8 +89,12 @@ function Dashboard() {
       if (existestudent(res, firstName, lastName)) {
         //اگر دانشجو قبلا وجود داشت پیامی را نشان دهد
         console.log("alrady exist sudent");
-        NotificationManager.error("alrady exist sudent");
-
+        NotificationManager.error("اطلاعات از قبل وجود دارد");
+        setFirstName("")
+        setLastName("")
+        setYear("")
+        setDegree("")
+        setMajor("")
         setLoading(false);
       } else {
         const tax = await studentRegistryContract.createStudent(
@@ -102,15 +108,21 @@ function Dashboard() {
         const receipt = await tax.wait();
         const evnets = receipt.events[0].args;
         console.log("evnets", evnets);
-
         if (evnets["code"] === "ACTION_CONFIRME") {
-          NotificationManager.success("!تراکنش انجام شد");
+          NotificationManager.success("  اطلاعات با موفقیت ثبت گردید ");
+          setFirstName("")
+          setLastName("")
+          setYear("")
+          setDegree("")
+          setMajor("")
+          setLoading(false);
         
         }
         console.log(tax, "tax");
         //اطلاعات تراکنش را به کاربر نشان دهد
         console.log("evnets", evnets);
         console.log("receipt", receipt);
+        NotificationManager.success( `مدرک شما صادر گردید  شماره مدرک شما ${tokenId} `);
         setLoading(false);
       }
     } catch (err) {
@@ -118,17 +130,13 @@ function Dashboard() {
       if (err.code === "ACTION_REJECTED") {
         NotificationManager.error("!تراکنش پذیرفته نشد");
       }
+      else{
+        NotificationManager.error( `Erorr ${err}`);
+      }
     }
   };
 
-  const handelCreatenft = async (student) => {
-    console.log(
-      student.firstName,
-      student.lastName,
-      student.education.degree,
-      student.education.major,
-      parseInt(student.education.year)
-    );
+  const handelCreatenft = async (student,index) => {
     const metadata = generate_metadata(
       student.firstName,
       student.lastName,
@@ -143,24 +151,52 @@ function Dashboard() {
       student.firstName
     );
     const IpfsHash = response.data.IpfsHash;
-    console.log(IpfsHash);
-    const tx = await nftContract.mint(IpfsHash);
-    console.log(tx);
-    const receipt = await tx.wait();
-    console.log(receipt);
-    const tokenId = receipt.events[1].args[1];
-    console.log(parseInt(tokenId._hex, 16));
-    const tokenURI = await nftContract.tokenURI(tokenId);
-    console.log(tokenURI);
+    
+   
+    try {
+      const tx = await nftContract.mint(IpfsHash);
+      console.log(tx);
+      const receipt = await tx.wait();
+      await handelisissuedcertificateStudent(index,true);
+      const tokenId = receipt.events[1].args[1];
+      const tokenURI = await nftContract.tokenURI(tokenId);
+      console.log(parseInt(tokenId._hex, 16));
+      console.log(tokenURI);
+    } catch (error) {
+      
+      if(error.reason="execution reverted: Only the owner can call this function."){
+        NotificationManager.error("!آدرس مورد نظر قادر به ایجاد مدرک نمی باشد ");
+      }
+      else{
+        NotificationManager.error( `Erorr${error.reason}`);
+      }
+    }
   };
 
   const handleReadStudent = async () => {
-    // console.log(students);
     const res = await studentRegistryContract.getAallStudents();
     setStudents(res);
-    console.log("student: ", students);
+    console.log("student: ", res);
   };
-
+  const handelAddoperator=async(address)=>{
+    try {
+       await nftContract.addoperator(address);
+       NotificationManager.success('کاربر  با ادرس جدیدافزوده شد')
+       
+    } catch (error) {
+      console.log(error)
+      NotificationManager.error( `Erorr ${error}`)
+    }
+  }
+  const handelisissuedcertificateStudent=async (studentid,isissued)=>{
+    try {
+      const tax = await studentRegistryContract.isissuedcertificate(studentid,isissued);
+      const response=tax.wait();
+      console.log(response)
+    } catch (error) {
+      console.log(response)
+    }
+  }
   const getalltokenurl = async function () {
     const result = await nftContract.getAllTokenIdsAndUrls();
     settokenurls(result[1]);
@@ -174,41 +210,53 @@ function Dashboard() {
   };
   return (
     <div>
-      <div style={{direction:"ltr"}} className="d-flex justify-content-between align-content-center my-2">
-        <button
-          type="button"
-          className="btn btn-primary d-flex align-items-center  shadow"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal2"
-        >
-          انتقال
-                  </button>
-
-        <button
-          type="button"
-          className="btn btn-primary d-flex align-items-center shadow "
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <i class="fa fa-plus pe-1"></i>
-                  افزودن 
-        </button>
-      </div>
       <div className="container">
-        <div className="row">
-          {students !== null ? (
-            <Table students={students} funcs={{ handelCreatenft }} />
-          ) : (
-            <div
-              className="spinner-border text-light p-4 mx-auto my-3 "
-              role="status"
-            />
-          )}
-        </div>
+          <div style={{direction:"ltr"}} className="d-flex justify-content-between align-content-center my-2">
+            <button
+              type="button"
+              className="btn btn-primary d-flex align-items-center  shadow"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal2"
+            >
+              انتقال
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-primary d-flex align-items-center shadow "
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+            >
+              <i class="fa fa-plus pe-1"></i>
+                      افزودن 
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary d-flex  shadow "
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal3"
+            >
+              <i class="fa fa-plus pe-1"></i>
+                افزدون آدرس  
+            </button>
+          </div>
+          <div className="container">
+            <div className="row">
+              {students !== null ? (
+                <Table students={students} funcs={{ handelCreatenft }} />
+              ) : (
+                <div
+                  className="spinner-border text-light p-4 mx-auto my-3 "
+                  role="status"
+                />
+              )}
+            </div>
+          </div>
       </div>
 
-      <div className="row">
-        <div className="container">
+      <div className="container">
+        <div className="row mt-4">
+          <h3>مدارک صادر شده اخیر </h3>
           <NFTs />
         </div>
       </div>
@@ -235,6 +283,11 @@ function Dashboard() {
         transfer={transfer}
         funcs={{ from, setFrom, to, setTo, tokenId, setTokenId }}
       />
+    <ModalAddOwner
+     handelAddoperator={handelAddoperator}
+     loading={loading}
+     funcs={{setaddress,address}}
+    />
     </div>
   );
 }
---
+++ client/src/components/Modaladdoperator.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { DatePicker } from "react-advance-jalaali-datepicker";
+import moment from "jalali-moment";
+function DatePickerInput(props) {
+  return <input className="form-control" {...props} />;
+}
+
+
+function ModalAddOwner({ handelAddoperator, loading, funcs }) {
+  const {
+    setaddress,
+    address,
+    setLoading
+  } = funcs;
+ console.log(address)
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal3"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      style={{direction:"ltr"}}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    افزدون آدرس کیف پول برای تایید کردن 
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <main className="container">
+              <div className="col-md-7 col-lg-8 mx-auto">
+                <div className="row g-5">
+                  <form className="needs-validation was-validated" />
+                  <div className="row g-3 text-end">
+                    <div className="col-12">
+                      <label htmlFor="firstName" className="form-label">
+                        آدرس
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="firstName"
+                        placeholder=""
+                        required=""
+                        onChange={(e) => {
+                          // console.log(e.target.value)
+                          setaddress(e.target.value);
+                        }}
+                      />
+                      <div className="invalid-feedback">
+                        Valid Addres is required.
+                      </div>
+                    </div>
+                  </div>
+
+                  <hr className="my-4" />
+                </div>
+              </div>
+            </main>
+          </div>
+          <div className="modal-footer justify-content-start">
+            {loading && (
+              <div className="spinner-border text-primary " role="status" />
+            )}
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+              onClick={() => setLoading(false)}
+            >
+              بستن
+            </button>
+
+            <button
+              type="button"
+              className="btn btn-primary "
+              disabled={loading}
+              onClick={() => {
+                handelAddoperator(address);
+              }}
+            >
+              افزودن
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalAddOwner;
