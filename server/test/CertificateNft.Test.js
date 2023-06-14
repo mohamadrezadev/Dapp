b80ad0ed1f206bca6b495dc303a4ee0d80ce8277
mohamadreza kiani <mohamadrezakiani9@yahoo.com>
2023-06-14 22:56:57 +0330

    update server

--- server/test/CertificateNft.Test.js
+++ server/test/CertificateNft.Test.js
@@ -1,63 +1,62 @@
-// SPDX-License-Identifier: MIT
 
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+// const  Contract  =require( "ethers");
 
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const  Contract  =require( "ethers");
-// import { ethers } from "hardhat";
-// import { expect } from "chai";
-
-describe("CERTNFT", function () {
+// describe("CERTNFT", function () {
   
-    let nftContract;
-    let owner='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+//     let nftContract;
+//     let owner='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+//     let owneraddres='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
 
 
-  beforeEach(async () => {
-    const NFTContract=await ethers.getContractFactory("CERTNFT") 
-    nftContract = await NFTContract.deploy("My NFT", "MNFT", 10);
-    await nftContract.deployed();
-  });
+//   beforeEach(async () => {
+//     const NFTContract=await ethers.getContractFactory("CERTNFT") 
+//     nftContract = await NFTContract.deploy(owneraddres, "My NFT", "MNFT");
+//     await nftContract.deployed();
+//   });
+  
+//   it("should mint an NFT", async function () {
 
-  it("should mint an NFT", async function () {
-    const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
-    const receipt = await tx.wait();
+//     const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+//     const receipt = await tx.wait();
     
-    const tokenId = receipt.events[1].args[1];
+//     const tokenId = receipt.events[1].args[1];
    
-    const tokenURI = await nftContract.tokenURI(tokenId);
+//     const tokenURI = await nftContract.tokenURI(tokenId);
    
-    expect(tokenURI).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
-  });
+//     expect(tokenURI).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+//   });
 
-  it("should emit a mintnft event", async function () {
-    const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
-    const receipt = await tx.wait();
+//   it("should emit a mintnft event", async function () {
+//     const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+//     const receipt = await tx.wait();
  
-    var tokenId = receipt.events[1].args[1];
-    console.log(tokenId)
-    const event = receipt.events[1];
-    expect(event.event).to.equal("mintnft");
-    expect(event.args[0]).to.equal(owner);
-    expect(event.args[1]).to.equal(tokenId);
-    expect(event.args[2]).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
-    expect(event.args[3]).to.equal("minted nft ");
-  });
-  it("should return token url and tiken id nft minted",async function(){
-     const result=await nftContract.getAllTokenIdsAndUrls();
-     console.log(result)
-  });
-  it("should transfer nft another address",async function(){
-     const from='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-     const to='0xbd1Cfa5B535AFd040E2443080CFF9fC9B557AF57'
-    //  const approv=await nftContract.setApprovalForAll(from,true)
-     const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
-     const receipt = await tx.wait();
+//     var tokenId = receipt.events[1].args[1];
+//     console.log(tokenId)
+//     const event = receipt.events[1];
+//     expect(event.event).to.equal("mintnft");
+//     expect(event.args[0]).to.equal(owner);
+//     expect(event.args[1]).to.equal(tokenId);
+//     expect(event.args[2]).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+//     expect(event.args[3]).to.equal("minted nft ");
+//   });
+//   it("should return token url and tiken id nft minted",async function(){
+//      const result=await nftContract.getAllTokenIdsAndUrls();
+//      console.log(result)
+//   });
+//   it("should transfer nft another address",async function(){
+//      const from='0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
+//      const to='0xbd1Cfa5B535AFd040E2443080CFF9fC9B557AF57'
+//     //  const approv=await nftContract.setApprovalForAll(from,true)
+//      const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+//      const receipt = await tx.wait();
      
-     const tokenId = receipt.events[1].args[1];
-     const tx2=await nftContract.transferFrom(from,to,tokenId)
-     const receipt2=tx2.wait();
-    //  console.log(receipt2)
-  })
-
-});
+//      const tokenId = receipt.events[1].args[1];
+//      const tx2=await nftContract.transferFrom(from,to,tokenId)
+//      const receipt2=tx2.wait();
+//     //  console.log(receipt2)
+//   })
+  
+// });
+ 
---
+++ server/test/nft.test.js
@@ -0,0 +1,91 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("CERTNFT", function () {
+  
+    let nftContract;
+    let owner; // Address of the contract owner
+    let ownerAddress; // Address of the contract owner as a string
+
+    beforeEach(async () => {
+        // Get the contract owner's address
+        [owner] = await ethers.getSigners();
+        ownerAddress = owner.address;
+
+        // Deploy the contract
+        const NFTContract = await ethers.getContractFactory("CERTNFT");
+        nftContract = await NFTContract.deploy(ownerAddress, "My NFT", "MNFT");
+        await nftContract.deployed();
+    });
+
+    it("should mint an NFT", async function () {
+        // Mint a new NFT with a given URI
+        const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+        const receipt = await tx.wait();
+
+        // Get the ID of the newly minted NFT
+        const tokenId = receipt.events[1].args[1];
+
+        // Get the token URI of the newly minted NFT
+        const tokenURI = await nftContract.tokenURI(tokenId);
+
+        // Check that the token URI is correct
+        expect(tokenURI).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+    });
+
+    it("should emit a mintnft event", async function () {
+        // Mint a new NFT with a given URI
+        const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+        const receipt = await tx.wait();
+
+        // Get the ID of the newly minted NFT
+        const tokenId = receipt.events[1].args[1];
+
+        // Check that the mintnft event was emitted with the correct parameters
+        const event = receipt.events[1];
+        expect(event.event).to.equal("mintnft");
+        expect(event.args[0]).to.equal(ownerAddress);
+        expect(event.args[1]).to.equal(tokenId);
+        expect(event.args[2]).to.equal("https://ipfs.io/ipfs/QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+        expect(event.args[3]).to.equal("ACTION_CONFIRMED");
+    });
+
+    it("should return token IDs and URIs of all minted NFTs", async function () {
+        // Mint some NFTs
+        await nftContract.mint("Qm1");
+        await nftContract.mint("Qm2");
+        await nftContract.mint("Qm3");
+
+        // Get an array of all token IDs and URIs
+        const [tokenIds, tokenURIs] = await nftContract.getAllTokenIdsAndUrls();
+
+        // Check that the arrays have the correct length
+        expect(tokenIds.length).to.equal(3);
+        expect(tokenURIs.length).to.equal(3);
+        
+        // Check that the token IDs and URIs are correct
+        expect(parseInt(tokenIds[0], 16)).to.deep.equal(1);
+        expect(parseInt(tokenIds[1], 16)).to.deep.equal(2);
+        expect(parseInt(tokenIds[2], 16)).to.deep.equal(3);
+        expect(tokenURIs).to.deep.equal([
+            "https://ipfs.io/ipfs/Qm1",
+            "https://ipfs.io/ipfs/Qm2",
+            "https://ipfs.io/ipfs/Qm3"
+        ]);
+    });
+
+    it("should transfer ownership of an NFT", async function () {
+        // Mint a new NFT
+        const tx = await nftContract.mint("QmcJH2iYfQ1f9RJjAbf5X5aF4nJSC2GJqRiNKAUx3rSZf6");
+        const receipt = await tx.wait();
+        const tokenId = receipt.events[1].args[1];
+
+        // Transfer ownership of the NFT to a different address
+        const to = "0x1234567890123456789012345678901234567890"; // Example address
+        await nftContract.transferFrom(ownerAddress, to, tokenId);
+
+        // Check that the new owner is correct
+        const newOwner = await nftContract.ownerOf(tokenId);
+        expect(newOwner).to.equal(to);
+    });
+});
